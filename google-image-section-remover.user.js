0659b695a80c34c6fbebf1531f385ba95d660e7d
Daniel Niccoli <daniel.niccoli@gmail.com>
2022-08-30 22:23:28 +0200

    Create google-image-section-remover.user.js

---
+++ google-image-section-remover.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Google Image Section Remover
+// @description Removes the image section from the google search results, if it is displayed.
+// @author      Daniel Niccoli
+// @version     1.0
+// @namespace   https://github.com/danielniccoli/userscripts
+// @homepageURL https://github.com/danielniccoli/userscripts
+// @supportURL  https://github.com/danielniccoli/userscripts/issues
+// @downloadURL https://github.com/danielniccoli/userscripts/blob/main/google-image-section-remover.user.js
+// @match       https://www.google.com/search
+// ==/UserScript==
+
+const targetElement = document.getElementById("iur").closest("#rso > div").remove()
